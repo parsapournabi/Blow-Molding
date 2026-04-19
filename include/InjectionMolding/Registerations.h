245bfc3f3ba80810b8f0c8e558f6ea8f630cdc2b
Wearily <parsapournabi1382@gmail.com>
2026-04-19 22:19:03 +0330

    registering PlcIOItem, PlcModbusDevice for QML usage pattern

--- include/InjectionMolding/Registerations.h
+++ include/InjectionMolding/Registerations.h
@@ -7,6 +7,8 @@
 #include "SerialGlobal.h"
 #include "SerialConnection.h"
 #include "modbuscom.h"
+#include "plcmodbusdevice.h"
+#include "plcioitem.h"
 #include "servomodbusdevice.h"
 
 #define QT_REGISTER_METATYPE(type) \
@@ -17,14 +19,21 @@
 
 Q_DECLARE_METATYPE(AbstractModbusDevice*);
 Q_DECLARE_METATYPE(QList<AbstractModbusDevice*>);
+Q_DECLARE_METATYPE(QList<PlcIOItem*>);
+Q_DECLARE_METATYPE(PlcIOItem*);
+Q_DECLARE_METATYPE(QVector<bool>);
 
 static void registerTypes()
 {
+    QT_REGISTER_METATYPE(PlcIOItem*);
     QT_REGISTER_METATYPE(AbstractModbusDevice*);
     QT_REGISTER_METATYPE(ServoModbusDevice*);
     QT_REGISTER_METATYPE(QList<AbstractModbusDevice*>);
     QT_REGISTER_METATYPE(QList<ServoModbusDevice*>);
+    QT_REGISTER_METATYPE(QList<PlcIOItem>*);
+    QT_REGISTER_METATYPE(QVector<bool>);
 
+    QTQML_REGISTER_TYPE("CustomItems", PlcModbusDevice);
     QTQML_REGISTER_TYPE("CustomItems", ServoModbusDevice);
     QTQML_REGISTER_TYPE("CustomItems", SerialGlobal);
     QTQML_REGISTER_TYPE("CustomItems", SerialConnection);
